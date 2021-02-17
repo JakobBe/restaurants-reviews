d7ade44aceb40b1a8a1d96298c9d678b012a07c1
JakobBe <jakob@bethmann.de>
2021-02-17 09:34:28 +0000

    Scaffold restaurants with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating Restaurants...'
+
+20.times do
+	restaurant = Restaurant.new(
+		name: Faker::Restaurant.name,
+		address: Faker::Address.street_address,
+		stars: rand(1..5)
+	)
+
+	restaurant.save
+	puts "Created restaurant: #{restaurant.name}"
+end
+
+puts "Seeding done" 
